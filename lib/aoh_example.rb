acb5a40a3da114f41bf31d6e4ff271ca65d38581
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-08 02:50:04 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,10 +1,10 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
-DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G = { name:  "Kate Gompert", occupation: "None" }
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+DON_G = { name: => "Don Gately", occupation: => "Live-in Staff" }
+JOELLE_VD = { name: =>  "Joelle van Dyne", occupation: => "Radio Personality" }
+PAT_M =  { name:  => "Pat Monteseian", occupation: => "Staff" }
+KATE_G = { name:  => "Kate Gompert", occupation: => "None" }
+BRUCE_G = { name:  => "Bruce Green", occupation: => "Fan of Mildred" }
 
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
@@ -15,11 +15,11 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  array=[DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
-KATE_G = { name:  "Kate Gompert", occupation: "None" },
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
+  array=[DON_G = { name: => "Don Gately", occupation: => "Live-in Staff" },
+JOELLE_VD = { name: => "Joelle van Dyne", occupation: => "Radio Personality" },
+PAT_M =  { name: => "Pat Monteseian", occupation: => "Staff" },
+KATE_G = { name:  => "Kate Gompert", occupation: => "None" },
+BRUCE_G = { name:  => "Bruce Green", occupation: => "Fan of Mildred" }]
 end
 
 def aoh_lookup(aoh, row, key)
